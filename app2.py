0e7d20b3da0d51d8e0da6174f446239a4df209ca
maria-carmen.brevia <maria-carmen.brevia@efrei.net>
2022-11-02 10:21:37 +0100

    commit update

--- app2.py
+++ app2.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, MCB!</p>"
+    return "<p>Hello, MCB... modification du code OK!</p>"
 
 if __name__ == "__main__":
 
